e979199f0efa343c5ac6381b1fa7d56eb196a7d3
Brian Vaughn <brian.david.vaughn@gmail.com>
2015-11-25 11:02:01 -0800

    Rewrote tests for Tape syntax. Temporarily commented out the SearchIndexWorker import statement in Main since it causes tests to fail.

--- source/SearchApi.test.js
+++ source/SearchApi.test.js
@@ -1,26 +1,25 @@
+import test from 'tape'
 import { SearchApi } from './SearchApi'
-import expect from 'expect.js'
+
+function getSearchApi () {
+  const documentA = {id: 1, name: 'One', description: 'The first document'}
+  const documentB = {id: 2, name: 'Two', description: 'The second document'}
+  const documentC = {id: 3, name: 'Three', description: 'The third document'}
+
+  const searchApi = new SearchApi() // Single-threaded Search API for easier testing
+  searchApi.indexResource(
+    'documents',
+    ['name', 'description'],
+    [ documentA, documentB, documentC ]
+  )
+  return searchApi
+}
 
 /** Simple smoke test of non-web-worker based SearchApi */
-describe('SearchApi', () => {
-  function getSearchApi () {
-    const documentA = {id: 1, name: 'One', description: 'The first document'}
-    const documentB = {id: 2, name: 'Two', description: 'The second document'}
-    const documentC = {id: 3, name: 'Three', description: 'The third document'}
-
-    const searchApi = new SearchApi() // Single-threaded Search API for easier testing
-    searchApi.indexResource(
-      'documents',
-      ['name', 'description'],
-      [ documentA, documentB, documentC ]
-    )
-    return searchApi
-  }
-
-  it('should return documents ids for any searchable field matching a query', async () => {
-    const searchApi = getSearchApi()
-    const ids = await searchApi.performSearch('documents', 'One')
-    expect(ids.length).to.equal(1)
-    expect(ids[0]).to.equal(1)
-  })
+test('SearchApi should return documents ids for any searchable field matching a query', async t => {
+  const searchApi = getSearchApi()
+  const ids = await searchApi.performSearch('documents', 'One')
+  t.equal(ids.length, 1)
+  t.equal(ids[0], 1)
+  t.end()
 })
--- source/lib/worker/Main.test.js
+++ source/lib/worker/Main.test.js
@@ -1,129 +1,129 @@
-import SearchIndexWorker from './SearchIndexWorker'
-import expect from 'expect.js'
+import test from 'tape'
+import SearchWorkerLoader from './Main'
 
-describe('SearchIndexWorker', () => {
-  class StubWorker {
-    constructor () {
-      this.indexedDocumentMap = {}
-      this.searchQueue = []
-    }
+class StubWorker {
+  constructor () {
+    this.indexedDocumentMap = {}
+    this.searchQueue = []
+  }
 
-    postMessage (data) {
-      const { method, ...props } = data
+  postMessage (data) {
+    const { method, ...props } = data
 
-      switch (method) {
-        case 'indexDocument':
-          const { uid, text } = props
-          if (!this.indexedDocumentMap[uid]) {
-            this.indexedDocumentMap[uid] = []
-          }
-          this.indexedDocumentMap[uid].push(text)
-          break
-        case 'search':
-          const { callbackId, query } = props
-          this.searchQueue.push({ callbackId, query })
-          break
-      }
-    }
-
-    rejectSearch (index, error) {
-      const { callbackId } = this.searchQueue[index]
-      this.onerror({ data: { error, callbackId } })
-    }
-
-    resolveSearch (index, results) {
-      const { callbackId } = this.searchQueue[index]
-      this.onmessage({ data: { results, callbackId } })
+    switch (method) {
+      case 'indexDocument':
+        const { uid, text } = props
+        if (!this.indexedDocumentMap[uid]) {
+          this.indexedDocumentMap[uid] = []
+        }
+        this.indexedDocumentMap[uid].push(text)
+        break
+      case 'search':
+        const { callbackId, query } = props
+        this.searchQueue.push({ callbackId, query })
+        break
     }
   }
 
-  let search
+  rejectSearch (index, error) {
+    const { callbackId } = this.searchQueue[index]
+    this.onerror({ data: { error, callbackId } })
+  }
 
-  beforeEach(() => {
-    search = new SearchIndexWorker(StubWorker)
-  })
+  resolveSearch (index, results) {
+    const { callbackId } = this.searchQueue[index]
+    this.onmessage({ data: { results, callbackId } })
+  }
+}
 
-  describe('indexDocument', () => {
-    it('should index a document with the specified text(s)', () => {
-      search.indexDocument('a', 'cat')
-      search.indexDocument('a', 'dog')
-      search.indexDocument('b', 'cat')
+test('SearchWorkerLoader indexDocument should index a document with the specified text(s)', t => {
+  const search = new SearchWorkerLoader(StubWorker)
+  search.indexDocument('a', 'cat')
+  search.indexDocument('a', 'dog')
+  search.indexDocument('b', 'cat')
 
-      expect(Object.keys(search.worker.indexedDocumentMap).length).to.equal(2)
-      expect(search.worker.indexedDocumentMap.a.length).to.equal(2)
-      expect(search.worker.indexedDocumentMap.a).to.eql(['cat', 'dog'])
-      expect(search.worker.indexedDocumentMap.b.length).to.equal(1)
-      expect(search.worker.indexedDocumentMap.b).to.eql(['cat'])
-    })
-  })
+  t.equal(Object.keys(search.worker.indexedDocumentMap).length, 2)
+  t.equal(search.worker.indexedDocumentMap.a.length, 2)
+  t.deepLooseEqual(search.worker.indexedDocumentMap.a, ['cat', 'dog'])
+  t.equal(search.worker.indexedDocumentMap.b.length, 1)
+  t.deepLooseEqual(search.worker.indexedDocumentMap.b, ['cat'])
+  t.end()
+})
 
-  describe('indexDocument', () => {
-    it('should search for the specified text', () => {
-      search.search('cat')
-      expect(search.worker.searchQueue.length).to.equal(1)
-      expect(search.worker.searchQueue[0].query).to.equal('cat')
-    })
+test('SearchWorkerLoader search should search for the specified text', t => {
+  const search = new SearchWorkerLoader(StubWorker)
+  search.search('cat')
+  t.equal(search.worker.searchQueue.length, 1)
+  t.equal(search.worker.searchQueue[0].query, 'cat')
+  t.end()
+})
 
-    it('should resolve the returned Promise on search completion', async () => {
-      const promise = search.search('cat')
-      search.worker.resolveSearch(0, ['a', 'b'])
+test('SearchWorkerLoader search should resolve the returned Promise on search completion', async t => {
+  const search = new SearchWorkerLoader(StubWorker)
+  const promise = search.search('cat')
+  search.worker.resolveSearch(0, ['a', 'b'])
 
-      const result = await promise
-      expect(result).to.eql(['a', 'b'])
-    })
+  const result = await promise
+  t.deepLooseEqual(result, ['a', 'b'])
+  t.end()
+})
 
-    it('should resolve multiple concurrent searches', async () => {
-      const promises = Promise.all([
-        search.search('cat'),
-        search.search('dog')
-      ])
-      search.worker.resolveSearch(0, ['a'])
-      search.worker.resolveSearch(1, ['a', 'b'])
-      await promises
-    })
+test('SearchWorkerLoader search should resolve multiple concurrent searches', async t => {
+  const search = new SearchWorkerLoader(StubWorker)
+  const promises = Promise.all([
+    search.search('cat'),
+    search.search('dog')
+  ])
+  search.worker.resolveSearch(0, ['a'])
+  search.worker.resolveSearch(1, ['a', 'b'])
+  await promises
+  t.end()
+})
 
-    it('should resolve searches in the correct order', async () => {
-      const results = []
-      const promiseList = [
-        search.search('cat'),
-        search.search('dog'),
-        search.search('rat')
-      ].map(promise => promise.then(result => results.push(result)))
+test('SearchWorkerLoader search should resolve searches in the correct order', async t => {
+  const search = new SearchWorkerLoader(StubWorker)
+  const results = []
+  const promiseList = [
+    search.search('cat'),
+    search.search('dog'),
+    search.search('rat')
+  ].map(promise => promise.then(result => results.push(result)))
 
-      search.worker.resolveSearch(1, ['1'])
-      search.worker.resolveSearch(0, ['0'])
-      search.worker.resolveSearch(2, ['2'])
+  search.worker.resolveSearch(1, ['1'])
+  search.worker.resolveSearch(0, ['0'])
+  search.worker.resolveSearch(2, ['2'])
 
-      await Promise.all(promiseList)
-      const [r1, r2, r3] = results
-      expect(r1).to.eql(['0'])
-      expect(r2).to.eql(['1'])
-      expect(r3).to.eql(['2'])
-    })
+  await Promise.all(promiseList)
+  const [r1, r2, r3] = results
+  t.deepLooseEqual(r1, ['0'])
+  t.deepLooseEqual(r2, ['1'])
+  t.deepLooseEqual(r3, ['2'])
+  t.end()
+})
 
-    it('should not reject all searches if one fails', async () => {
-      const errors = []
-      const results = []
-      const promises = [
-        search.search('cat'),
-        search.search('dog')
-      ].map(promise =>
-        promise
-          .then(result => results.push(result))
-          .catch(error => errors.push(error))
-      )
+test('SearchWorkerLoader search should not reject all searches if one fails', async t => {
+  const search = new SearchWorkerLoader(StubWorker)
+  const errors = []
+  const results = []
+  const promises = [
+    search.search('cat'),
+    search.search('dog')
+  ].map(promise =>
+    promise
+      .then(result => results.push(result))
+      .catch(error => errors.push(error))
+  )
 
-      search.worker.rejectSearch(1, new Error('1'))
-      search.worker.resolveSearch(0, ['0'])
+  search.worker.rejectSearch(1, new Error('1'))
+  search.worker.resolveSearch(0, ['0'])
 
-      try {
-        await Promise.all(promises)
-      } catch (err) {}
+  try {
+    await Promise.all(promises)
+  } catch (err) {}
 
-      expect(results.length).to.equal(1)
-      expect(results[0]).to.eql(['0'])
-      expect(errors.length).to.equal(1)
-      expect(errors[0].message).to.equal('1')
-    })
-  })
+  t.equal(results.length, 1)
+  t.deepLooseEqual(results[0], ['0'])
+  t.equal(errors.length, 1)
+  t.equal(errors[0].message, '1')
+  t.end()
 })
