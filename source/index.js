d95020a6e8aa42d1dea3f708b7a89169bf1b739c
Cesar Andreu <cesarandreu@gmail.com>
2015-11-24 18:47:31 -0800

    Initial commit of redux code

--- source/index.js
+++ source/index.js
@@ -1,3 +1,10 @@
 /* @flow */
+export {
+  defaultSearchStateSelector,
+  getSearchSelectors
+} from './selectors'
+export { search } from './actions'
+export reduxSearch from './reduxSearch'
+export reducer from './reducer'
 export { SearchApi } from './SearchApi'
 export { WorkerSearchApi } from './SearchApi'
